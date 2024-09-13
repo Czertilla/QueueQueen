e38be9f000dd78c5bd5675c202859842c784893d
Czertilla <blaztermapskorsak@gmail.com>
2024-09-13 22:37:18 +0300

    chore()

--- docker/app.sh
+++ docker/app.sh
@@ -4,4 +4,4 @@ alembic upgrade head
 
 export PYTHONPATH=src 
 
-gunicorn src.main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8080
+gunicorn src.main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
