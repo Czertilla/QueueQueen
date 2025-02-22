fe961915230fe76f79d6a60b514ab4173848e76e
Czertilla <blaztermapskorsak@gmail.com>
2025-02-23 00:25:33 +0300

    refactor(bot) remove class

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,12 +1,13 @@
-from utils.mixins.singleton import SingletonMixin
-from aiogram import Bot as Base, Dispatcher
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
+from src.utils.settings import getSettings
+# from .middlewares import register_middlewares
+from .routers import register_routers
 
-from utils.settings import Settings
+settings = getSettings()
+bot = Bot(token=settings.BOT_TG_TOKEN, default=DefaultBotProperties(parse_mode=settings.BOT_PARSE_MODE))
+dp = Dispatcher(storage=MemoryStorage())
 
-
-class Bot(Base, SingletonMixin):
-    def __init__(self):
-        super().__init__(token=Settings().BOT_TG_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-        self.dp = Dispatcher()
+# register_middlewares(dp)
+register_routers(dp)
