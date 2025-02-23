50a5c6f2065d16a24033e39c68a06a0f5398f19c
Czertilla <blaztermapskorsak@gmail.com>
2025-02-24 01:51:51 +0300

    chore(polling) add root logger

--- tests/bot/polling.py
+++ tests/bot/polling.py
@@ -1,6 +1,17 @@
 import asyncio
 import sys
 import os
+from logging import Formatter, StreamHandler, getLogger, DEBUG
+
+logger = getLogger()
+logger.setLevel(DEBUG)
+if not logger.hasHandlers():
+    handler = StreamHandler()  # Вывод в консоль (stdout)
+    formatter = Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
 parent_directory = os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..', '..')
@@ -8,7 +19,6 @@ parent_directory = os.path.abspath(
 sys.path.append(parent_directory)
 
 from src.bot import bot, dp
-
 
 
 if __name__ == "__main__":
