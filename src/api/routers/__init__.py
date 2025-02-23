bf97d3d0a919c4fd0d45da828f560a56751e0dc1
Czertilla <blaztermapskorsak@gmail.com>
2025-02-23 15:13:51 +0300

    chore(api.routers) use load_common

--- src/api/routers/__init__.py
+++ src/api/routers/__init__.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
-from .webhook import router as webhook_router
-routers = (
-    webhook_router,
-)
+from fastapi import FastAPI, APIRouter
+from utils.imports import load_common
+
+
+routers = load_common(__name__, 'router', APIRouter)
 
 
 def include_routers(app: FastAPI) -> None:
