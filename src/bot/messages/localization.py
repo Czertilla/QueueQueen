db138a281ddfc2df7d5f65e90bacbe332638c68e
Czertilla <blaztermapskorsak@gmail.com>
2025-02-24 01:49:06 +0300

    chore(Localization) add logging

--- src/bot/messages/localization.py
+++ src/bot/messages/localization.py
@@ -1,10 +1,14 @@
+import logging
 import i18n
-import asyncio
 import aiocache
 from pathlib import Path
 from typing import Any, Optional
 
 from utils.settings import getSettings
+
+# Configure logging
+logger = logging.getLogger(__name__)
+
 
 class Localization:
     cache: aiocache.Cache = aiocache.Cache(aiocache.SimpleMemoryCache)
@@ -13,26 +17,38 @@ class Localization:
     def __init__(self, default_lang: str = "en", ttl: int = TTL) -> None:
         """
         Initializes the Localization object.
-        
+
         :param default_lang: The default language for localization (default is 'en')
         :param ttl: Time-to-live for cache entries (default is TTL from settings)
         """
         self.default_lang: str = default_lang
         self.translations_path: Path = Path(__file__).parent / "locales"
         self.ttl: int = ttl
-        
+
         assert isinstance(i18n.load_path, list)
         i18n.load_path.append(str(self.translations_path))
         i18n.set("fallback", self.default_lang)
 
+        logger.info(
+            f"Localization initialized  {self=}")
+        
+    
+    def __repr__(self) -> str:
+        """
+        Returns a string representation of the Localization object.
+
+        :return: A string containing class name, default language, and TTL.
+        """
+        return f"<Localization(default_lang={self.default_lang}, ttl={self.ttl})>"
+
     async def get(
-            self, key: str, lang: Optional[str] = None, 
+            self, key: str, lang: Optional[str] = None,
             markup: str = "HTML", **kwargs: Any
     ) -> str:
         """
         Asynchronously retrieves the localization string from cache or loads 
         it if not cached.
-        
+
         :param key: The translation key
         :param lang: The language to retrieve the translation in 
             (default is None, falls back to default_lang)
@@ -46,15 +62,22 @@ class Localization:
         # Checking the cache
         cached_translation: Optional[str] = await self.cache.get(cache_key)
         if cached_translation:
+            logger.debug(f"Cache hit: {cache_key}")
             return cached_translation
+
+        # Cache miss
+        logger.debug(f"Cache miss: {cache_key}. Loading translation.")
 
         # Uploading the translation
         i18n.set("locale", lang)
         text: str = i18n.t(f"{markup}.{key}", **kwargs)
 
-        # We put it in the cache with TTL = 5 minutes
+        # We put it in the cache with TTL
         await self.cache.set(cache_key, text, ttl=self.ttl)
+        logger.debug(f"Cached new translation: {cache_key} (TTL: {self.ttl}s)")
+
         return text
+
 
 # Global localization object
 i18n_manager: Localization = Localization()
