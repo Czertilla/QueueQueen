b769eb9cd54d83368390a2c7fc69cf2a2ba44e70
Czertilla <blaztermapskorsak@gmail.com>
2025-02-23 16:02:46 +0300

    chore(bot) register routers

--- src/bot/routers/__init__.py
+++ src/bot/routers/__init__.py
@@ -1,9 +1,7 @@
 from aiogram import Dispatcher, Router
-from importlib import __import__
-routers: list[Router] = [
+from utils.imports import load_common
 
-]
-
+routers = load_common(__name__, 'router', Router)
 
 def register_routers(dp: Dispatcher) -> None:
     dp.include_routers(*routers)
