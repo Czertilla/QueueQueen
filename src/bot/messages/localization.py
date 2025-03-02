ad345e63075675a1c62fba334453db8206f58c48
Czertilla <blaztermapskorsak@gmail.com>
2025-03-03 02:21:37 +0300

    perf(localization) refactor docstrings to googl format

--- src/bot/messages/localization.py
+++ src/bot/messages/localization.py
@@ -11,33 +11,43 @@ logger = logging.getLogger(__name__)
 
 
 class Localization:
+    """
+    Handles localization and caching for translated strings.
+
+    Attributes:
+        cache (aiocache.Cache): cache instanse
+        TTL (int): default Time-to-live for cached translations from settings
+            Defaults to `TTL_SECONDS` from settings
+    """
+
     cache: aiocache.Cache = aiocache.Cache(aiocache.SimpleMemoryCache)
     TTL: int = getSettings().TTL_SECONDS
 
     def __init__(self, default_lang: str = "en", ttl: int = TTL) -> None:
         """
-        Initializes the Localization object.
+        Initializes the Localization instance.
 
-        :param default_lang: The default language for localization (default is 'en')
-        :param ttl: Time-to-live for cache entries (default is TTL from settings)
+        Args:
+            default_lang (str, optional): Default language for translations.
+                Defaults to "en".
+            ttl (int, optional): Time-to-live for cached translations.
+                Defaults to `TTL` attribute.
         """
         self.default_lang: str = default_lang
         self.translations_path: Path = Path(__file__).parent / "locales"
         self.ttl: int = ttl
 
-        assert isinstance(i18n.load_path, list)
         i18n.load_path.append(str(self.translations_path))
         i18n.set("fallback", self.default_lang)
 
-        logger.info(
-            f"Localization initialized  {self=}")
-        
-    
+        logger.info(f"Localization initialized: {self}")
+
     def __repr__(self) -> str:
         """
-        Returns a string representation of the Localization object.
+        Returns a string representation of the Localization instance.
 
-        :return: A string containing class name, default language, and TTL.
+        Returns:
+            str: A formatted string containing the class name, default language, and TTL.
         """
         return f"<Localization(default_lang={self.default_lang}, ttl={self.ttl})>"
 
@@ -46,35 +56,37 @@ class Localization:
             markup: str = "HTML", **kwargs: Any
     ) -> str:
         """
-        Asynchronously retrieves the localization string from cache or loads 
-        it if not cached.
+        Retrieves a localized string asynchronously. Uses caching to improve performance.
 
-        :param key: The translation key
-        :param lang: The language to retrieve the translation in 
-            (default is None, falls back to default_lang)
-        :param markup: The format for the translation key (default is "HTML")
-        :param kwargs: Additional arguments for translation formatting
-        :return: Translated string
+        Args:
+            key (str): The translation key.
+            lang (str, optional): The language for the translation.
+                Defaults to `None`, which falls back to `default_lang`.
+            markup (str, optional): The format for the translation key.
+                Defaults to "HTML".
+            **kwargs (Any): Additional parameters for formatting.
+
+        Returns:
+            str: The translated string.
         """
         lang = lang or self.default_lang
         cache_key = f"i18n:{lang}:{key}"
 
-        # Checking the cache
+        # Check cache
         cached_translation: Optional[str] = await self.cache.get(cache_key)
         if cached_translation:
             logger.debug(f"Cache hit: {cache_key}")
             return cached_translation
 
-        # Cache miss
-        logger.debug(f"Cache miss: {cache_key}. Loading translation.")
+        # Load translation if cache miss
+        logger.debug(f"Cache miss: {cache_key}. Fetching translation.")
 
-        # Uploading the translation
         i18n.set("locale", lang)
         text: str = i18n.t(f"{markup}.{key}", **kwargs)
 
-        # We put it in the cache with TTL
+        # Store in cache
         await self.cache.set(cache_key, text, ttl=self.ttl)
-        logger.debug(f"Cached new translation: {cache_key} (TTL: {self.ttl}s)")
+        logger.debug(f"Cached translation: {cache_key} (TTL: {self.ttl}s)")
 
         return text
 
