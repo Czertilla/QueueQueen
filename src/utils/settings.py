ed55c9e2493019469841b30f1dfa859de8961789
Czertilla <blaztermapskorsak@gmail.com>
2025-03-08 00:04:48 +0300

    feature(settings) add confitional fileds add atributes for settings if DB_DBMS is 'postgres' add docstrings add Enums

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -11,7 +11,8 @@ class Settings(BaseSettings):
     """
     Application settings class.
 
-    Loads settings from environment variables and .env file.
+    This class loads settings from environment variables and the .env file.
+    It is used to manage application configuration parameters.
     """
 
     APP_NAME: str = "FASAPI APP"
@@ -19,26 +20,48 @@ class Settings(BaseSettings):
 
     DB_DBMS: DBManagerType
     """Type of database management system used (e.g., sqlite, postgres)."""
+
     DB_NAME: str
-    """Name of database used."""
+    """Name of the database used."""
+
+    if DB_DBMS == DBManagerType.postgres:
+        DB_USER: str
+        """Username for connecting to the database."""
+        
+        DB_PASS: str
+        """Password for the database user."""
+
+        DB_HOST: str
+        """Hostname or IP address of the database server."""
+
+        DB_PORT: str
+        """Port number on which the database server is running."""
 
     TTL_SECONDS: int = 300
-    """TTL cache storage time in seconds."""
+    """Time-to-live (TTL) for cache storage, in seconds."""
 
     BOT_TG_TOKEN: str
-    """Token of the Telegram bot, which is issued by BotFather.
-    For more information, visit https://core.telegram.org/bots."""
+    """Telegram bot token issued by BotFather.
+    
+    For more information, visit: https://core.telegram.org/bots
+    """
+
     BOT_TG_WEBHOOK: str
-    """The HTTPS address of your application (WITHOUT THE REQUEST PATH) that Telegram for webhook will use."""
+    """The HTTPS address of your application (WITHOUT THE REQUEST PATH) that Telegram will use for the webhook."""
+
     BOT_PARSE_MODE: BotParserType
+    """The message parsing mode for the Telegram bot (e.g., Markdown, HTML)."""
 
     model_config = SettingsConfigDict(env_file=environ, extra="ignore")
+    """Configuration for Pydantic settings, defining how environment variables are loaded."""
 
 
 @lru_cache
 def getSettings() -> Settings:
     """
     Returns a cached instance of the application settings.
+
+    This function ensures that the settings are only loaded once and then reused.
 
     Returns:
         Settings: The application settings instance.
