0d2eb1bf451db0cc0a909269ca1532ce139109c4
Czertilla <blaztermapskorsak@gmail.com>
2025-03-08 00:28:56 +0300

    chore(enums.settings) remove ParserMode

--- src/utils/enums/settings.py
+++ src/utils/enums/settings.py
@@ -1,12 +1,7 @@
 from utils.abstract.enum import AEnum
-from aiogram.enums import ParseMode
 
 class DBManagerType(str, AEnum):
     sqlite = "sqlite"
     postgres = "postgres"
 
     __default__ = sqlite
-
-
-class BotParserType(AEnum, ParseMode):
-    __default__ = ParseMode.HTML
