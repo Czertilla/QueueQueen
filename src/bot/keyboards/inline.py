c3795bcb0fae7a33a8363c0537fa7320e85e2dc9
Czertilla <blaztermapskorsak@gmail.com>
2025-03-12 19:13:03 +0300

    docs(InlineBuilder):

--- src/bot/keyboards/inline.py
+++ src/bot/keyboards/inline.py
@@ -8,22 +8,25 @@ from utils.enums.locales import LocaleKey
 
 class InlineBuilder(InlineKeyboardBuilder):
     def __init__(self, text_builder: MessageTextBuilder, *args, **kwargs):
-        """_summary_
+        """
+        Custom inline keyboard builder with localization support.
 
         Args:
-            text_builder (MessageTextBuilder): _description_
+            text_builder (MessageTextBuilder): An instance for getting 
+            localized phrases.   
         """
         super().__init__(*args, **kwargs)
         self.text_builder = text_builder
 
-    async def quit_kb(self, chat_id: int):
-        """_summary_
+    async def quit_kb(self, chat_id: int) -> InlineKeyboardMarkup:
+        """
+        Creates an inline keyboard with a button to exit the chat.
 
         Args:
-            chat_id (int): _description_
+            chat_id (int): The chat ID.
 
         Returns:
-            _type_: _description_
+            InlineKeyboardMarkup: An inline keyboard object.
         """
         buttons = [
             [
@@ -37,11 +40,16 @@ class InlineBuilder(InlineKeyboardBuilder):
         ]
         return InlineKeyboardMarkup(inline_keyboard=buttons)
 
-    async def kick_kb(self, target_id: int):
-        """_summary_
+    async def kick_kb(self, target_id: int) -> InlineKeyboardMarkup:
+        """
+        Creates an inline keyboard to confirm the user's kick.
 
         Args:
-            target_id (int): _description_
+            target_id (int): The ID of the user to kick.
+
+        Returns:
+            InlineKeyboardMarkup: An inline keyboard object with undo and 
+            kick buttons.
         """
         buttons = [
             [
