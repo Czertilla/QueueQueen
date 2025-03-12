9683b55ee3593e7ea233887f0d43216442d2865c
Czertilla <blaztermapskorsak@gmail.com>
2025-03-12 18:33:13 +0300

    feat(InlineBuilder): :bento: add kick_kb mthd

--- src/bot/keyboards/inline.py
+++ src/bot/keyboards/inline.py
@@ -8,10 +8,23 @@ from utils.enums.locales import LocaleKey
 
 class InlineBuilder(InlineKeyboardBuilder):
     def __init__(self, text_builder: MessageTextBuilder, *args, **kwargs):
+        """_summary_
+
+        Args:
+            text_builder (MessageTextBuilder): _description_
+        """
         super().__init__(*args, **kwargs)
         self.text_builder = text_builder
 
     async def quit_kb(self, chat_id: int):
+        """_summary_
+
+        Args:
+            chat_id (int): _description_
+
+        Returns:
+            _type_: _description_
+        """
         buttons = [
             [
                 InlineKeyboardButton(
@@ -19,6 +32,30 @@ class InlineBuilder(InlineKeyboardBuilder):
                         LocaleKey.quit_button
                     ),
                     callback_data=f"{CallbackPrefix.quit.value}{chat_id}"
+                )
+            ]
+        ]
+        return InlineKeyboardMarkup(inline_keyboard=buttons)
+
+    async def kick_kb(self, target_id: int):
+        """_summary_
+
+        Args:
+            target_id (int): _description_
+        """
+        buttons = [
+            [
+                InlineKeyboardButton(
+                    text=await self.text_builder.get_phrase(
+                        LocaleKey.cansel_button
+                    ),
+                    callback_data=CallbackPrefix.cansel.value
+                ),
+                InlineKeyboardButton(
+                    text=await self.text_builder.get_phrase(
+                        LocaleKey.kick_button
+                    ),
+                    callback_data=f"{CallbackPrefix.kick.value}{target_id}"
                 )
             ]
         ]
